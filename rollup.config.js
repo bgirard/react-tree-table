aab91f813fd74c42d1dda8b4ca887242f8e53903
Benoit Girard <benoit.girard@databricks.com>
2021-08-29 16:03:17 -0700

    Almost working examples file

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import flow from 'rollup-plugin-flow';
 import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import url from '@rollup/plugin-url';
 
@@ -14,7 +15,7 @@ export default {
     {
       file: 'bundle.min.js',
       name: 'ReactTreeTable',
-      format: 'iife',
+      format: 'cjs',
     }
   ],
   plugins: [
@@ -25,6 +26,9 @@ export default {
     }),
     babel({
       exclude: 'node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': 'true'
     }),
     resolve(),
     commonjs(),
